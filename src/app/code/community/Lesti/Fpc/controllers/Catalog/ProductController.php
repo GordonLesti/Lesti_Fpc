1d36e66d66aa339416b31d7e0a265a9ccc88b426
Gordon Lesti <info@gordonlesti.com>
2014-09-14 00:41:38 +0200

    change license comment blocks

--- src/app/code/community/Lesti/Fpc/controllers/Catalog/ProductController.php
+++ src/app/code/community/Lesti/Fpc/controllers/Catalog/ProductController.php
@@ -1,16 +1,14 @@
 <?php
 /**
- * Lesti_Fpc
+ * Lesti_Fpc (http:gordonlesti.com/lestifpc)
  *
- * NOTICE OF LICENSE
+ * PHP version 5
  *
- * This source file is subject to the Open Software License (OSL 3.0)
- * http://opensource.org/licenses/OSL-3.0
- *
- * @package      Lesti_Fpc
- * @copyright    Copyright (c) 2013 Gordon Lesti (http://www.gordonlesti.com)
- * @author       Gordon Lesti <info@gordonlesti.com>
- * @license      http://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * @link      https://github.com/GordonLesti/Lesti_Fpc
+ * @package   Lesti_Fpc
+ * @author    Gordon Lesti <info@gordonlesti.com>
+ * @copyright Copyright (c) 2013-2014 Gordon Lesti (http://gordonlesti.com)
+ * @license   http://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  */
 
 /**
@@ -18,10 +16,6 @@
  */
 class Lesti_Fpc_Catalog_ProductController extends Mage_Core_Controller_Front_Action
 {
-
-    /**
-     *
-     */
     public function viewAction()
     {
         $productId = (int)Mage::app()->getRequest()->getParam('id');
