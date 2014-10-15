c8101e8c83a9c6fb48fe221bec7a9502cd9bb4d4
Gordon Lesti <info@gordonlesti.com>
2014-10-15 21:28:09 +0200

    transport tests from tests into Lesti/Fpc/Test

---
+++ src/app/code/community/Lesti/Fpc/Test/Block/Catalog/Product/View/Ajax.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Lesti_Fpc (http:gordonlesti.com/lestifpc)
+ *
+ * PHP version 5
+ *
+ * @link      https://github.com/GordonLesti/Lesti_Fpc
+ * @package   Lesti_Fpc
+ * @author    Gordon Lesti <info@gordonlesti.com>
+ * @copyright Copyright (c) 2013-2014 Gordon Lesti (http://gordonlesti.com)
+ * @license   http://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ */
+
+/**
+ * Class Lesti_Fpc_Test_Block_Catalog_Product_View_Ajax
+ */
+class Lesti_Fpc_Test_Block_Catalog_Product_View_Ajax extends Lesti_Fpc_Test_TestCase
+{
+    /**
+     * @var Lesti_Fpc_Block_Catalog_Product_View_Ajax
+     */
+    protected $_catalogProductViewAjaxBlock;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->_catalogProductViewAjaxBlock = new Lesti_Fpc_Block_Catalog_Product_View_Ajax();
+        // register a product
+        $product = new Mage_Catalog_Model_Product();
+        $product->setId(5);
+        Mage::register('current_product', $product);
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+        Mage::unregister('current_product');
+    }
+
+    /**
+     * @test
+     * @loadFixture get_ajax_url_fpc_inactive.yaml
+     */
+    public function testGetAjaxUrlFpcInactive()
+    {
+        Mage::app()->getCacheInstance()->banUse('fpc');
+        $this->assertFalse($this->_catalogProductViewAjaxBlock->getAjaxUrl());
+    }
+
+    /**
+     * @test
+     * @loadFixture get_ajax_url_miss_cache_able_action.yaml
+     */
+    public function testGetAjaxUrlMissCacheAbleAction()
+    {
+        $this->assertFalse($this->_catalogProductViewAjaxBlock->getAjaxUrl());
+    }
+
+    /**
+     * @test
+     * @loadFixture get_ajax_url_disabled_recently_viewed_products.yaml
+     */
+    public function testGetAjaxUrlDisabledRecentlyViewedProducts()
+    {
+        $this->assertFalse($this->_catalogProductViewAjaxBlock->getAjaxUrl());
+    }
+
+    /**
+     * @test
+     * @loadFixture get_ajax_url.yaml
+     */
+    public function testGetAjaxUrl()
+    {
+        $this->assertEquals(
+            'http://localhost/fpc/catalog_product/view/id/5/',
+            $this->_catalogProductViewAjaxBlock->getAjaxUrl()
+        );
+    }
+}
---
+++ src/app/code/community/Lesti/Fpc/Test/TestCase.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Lesti_Fpc (http:gordonlesti.com/lestifpc)
+ *
+ * PHP version 5
+ *
+ * @link      https://github.com/GordonLesti/Lesti_Fpc
+ * @package   Lesti_Fpc
+ * @author    Gordon Lesti <info@gordonlesti.com>
+ * @copyright Copyright (c) 2013-2014 Gordon Lesti (http://gordonlesti.com)
+ * @license   http://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ */
+
+/**
+ * Class Lesti_Fpc_Test_TestCase
+ */
+class Lesti_Fpc_Test_TestCase extends EcomDev_PHPUnit_Test_Case_Controller
+{
+    protected $_cacheOptions;
+
+    protected $_cache;
+
+    public function setUp()
+    {
+        parent::setUp();
+        // unregister fpc
+        Mage::unregister('_singleton/fpc/fpc');
+        // disable all caches expected fpc
+        $this->_cache = Mage::app()->getCacheInstance();
+        $this->_cacheOptions = Mage::getResourceSingleton('core/cache')->getAllOptions();
+        $cacheOptions = $this->_cacheOptions;
+        foreach (array_keys($cacheOptions) as $cache) {
+            $cacheOptions[$cache] = $cache == 'fpc' ? 1 : 0;
+        }
+        if (!array_key_exists('fpc', $cacheOptions)) {
+            $cacheOptions['fpc'] = 1;
+        }
+        $this->_cache->saveOptions($cacheOptions);
+        $cacheReflector = new ReflectionClass('Mage_Core_Model_Cache');
+        $initOptionsMethod = $cacheReflector->getMethod('_initOptions');
+        $initOptionsMethod->setAccessible(true);
+        $initOptionsMethod->invokeArgs($this->_cache, array());
+
+        // clean baseUrlCache of Model/Store
+        $storeReflector = new ReflectionClass('Mage_Core_Model_Store');
+        $baseUrlCacheProperty = $storeReflector->getProperty('_baseUrlCache');
+        $baseUrlCacheProperty->setAccessible(true);
+        $baseUrlCacheProperty->setValue(Mage::app()->getStore(), array());
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+        $this->_cache->saveOptions($this->_cacheOptions);
+    }
+}
